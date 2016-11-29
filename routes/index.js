3f306dff798a92f764f4ffac542cf087124f4ecb
Joost Hietbrink <joost@joopp.com>
2016-11-29 13:20:38 +0100

    First working FB login.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 
@@ -13,16 +14,18 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var AWS = require('aws-sdk');
 var colors = require('colors');
 
-var AWS_ACCOUNT_ID = 'XXXXXXXXXXX';
-var AWS_Region = 'us-east-1';
-var COGNITO_IDENTITY_POOL_ID = 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx';
+var AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
+var AWS_REGION = process.env.AWS_REGION;
+var COGNITO_IDENTITY_POOL_ID = process.env.COGNITO_IDENTITY_POOL_ID;;
 var COGNITO_IDENTITY_ID, COGNITO_SYNC_TOKEN, AWS_TEMP_CREDENTIALS;
 var cognitosync;
-var IAM_ROLE_ARN = 'arn:aws:iam::xxxxxxxxxx:role/Cognito_AWSCognitoTutorialAuth_DefaultRole';
+var IAM_ROLE_ARN = process.env.IAM_ROLE_ARN;
 var COGNITO_SYNC_COUNT;
 var COGNITO_DATASET_NAME = 'TEST_DATASET';
-var FACEBOOK_APP_ID = 'xxxxxxxxxxxx';
-var FACEBOOK_APP_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+// test app
+var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 var FACEBOOK_TOKEN;
 var FACEBOOK_USER = {
   id: '',
@@ -92,5 +95,106 @@ router.route('/sync')
 router.get('/error', function(req, res, next) {
   res.send("Unable to access Facebook servers. Please check internet connection or try again later.");
 });
+
+function getCognitoID(){
+  // The parameters required to intialize the Cognito Credentials object.
+  var params = {
+    AccountId: AWS_ACCOUNT_ID, // required
+    RoleArn: IAM_ROLE_ARN,  // required
+    IdentityPoolId: COGNITO_IDENTITY_POOL_ID, // required
+    Logins: {
+      'graph.facebook.com': FACEBOOK_TOKEN
+    }
+  };
+  // set the Amazon Cognito region
+  AWS.config.region = AWS_REGION;
+  // initialize the Credentials object with our parameters
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials(params);
+
+  // We can set the get method of the Credentials object to retrieve
+  // the unique identifier for the end user (identityId) once the provider
+  // has refreshed itself
+  AWS.config.credentials.get(function(err) {
+    if (err) console.log("credentials.get: ".red + err, err.stack); // an error occurred
+      else{
+        //AWS_TEMP_CREDENTIALS = AWS.config.credentials.data.Credentials;
+        AWS_TEMP_CREDENTIALS = AWS.config.credentials;
+        COGNITO_IDENTITY_ID = AWS.config.credentials.identityId;
+        console.log("Cognito Identity Id: ".green + COGNITO_IDENTITY_ID);
+        getCognitoSynToken();
+      }
+  });
+}
+
+function getCognitoSynToken(){
+  // Other AWS SDKs will automatically use the Cognito Credentials provider
+  // configured in the JavaScript SDK.
+  cognitosync = new AWS.CognitoSync();
+  cognitosync.listRecords({
+    DatasetName: COGNITO_DATASET_NAME, // required
+    IdentityId: COGNITO_IDENTITY_ID,  // required
+    IdentityPoolId: COGNITO_IDENTITY_POOL_ID  // required
+  }, function(err, data) {
+    if (err) console.log("listRecords: ".red + err, err.stack); // an error occurred
+      else {
+        console.log("listRecords: ".green + JSON.stringify(data));
+        COGNITO_SYNC_TOKEN = data.SyncSessionToken;
+        COGNITO_SYNC_COUNT = data.DatasetSyncCount;
+        console.log("SyncSessionToken: ".green + COGNITO_SYNC_TOKEN);           // successful response
+        console.log("DatasetSyncCount: ".green + COGNITO_SYNC_COUNT);
+        addRecord();
+      }
+  });
+}
+
+function addRecord(){
+  var params = {
+    DatasetName: COGNITO_DATASET_NAME, // required
+    IdentityId: COGNITO_IDENTITY_ID, // required
+    IdentityPoolId: COGNITO_IDENTITY_POOL_ID, // required
+    SyncSessionToken: COGNITO_SYNC_TOKEN, // required
+    RecordPatches: [
+      {
+        Key: 'USER_ID', // required
+        Op: 'replace', // required
+        SyncCount: COGNITO_SYNC_COUNT, // required
+        //DeviceLastModifiedDate: new Date(),
+        Value: FACEBOOK_USER.id
+      }
+    ]
+  };
+  console.log("UserID: ".cyan + FACEBOOK_USER.id);
+  cognitosync.updateRecords(params, function(err, data) {
+    if (err) console.log("updateRecords: ".red + err, err.stack); // an error occurred
+    else{
+      console.log("Value: ".green + JSON.stringify(data));           // successful response
+      createS3();
+    }
+  });
+}
+
+function createS3(){
+  var bucket = new AWS.S3({
+    params: {
+      Bucket: 'backspace-cognito-test'
+    }
+  });
+  //Object key will be facebook-USERID#/FILE_NAME
+  console.log('COGNITO_IDENTITY_ID: '.cyan + COGNITO_IDENTITY_ID);
+  var objKey = COGNITO_IDENTITY_ID + '/test2.txt';
+  var params = {
+      Key: objKey,
+      ContentType: 'text/plain',
+      Body: "Hello!",
+      ACL: 'public-read'
+  };
+  bucket.putObject(params, function (err, data) {
+    if (err) {
+        console.log('putObject: '.red + err);
+    } else {
+        console.log("Successfully uploaded data to your S3 bucket");
+    }
+  });
+}
 
 module.exports = router;
